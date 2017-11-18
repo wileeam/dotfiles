55eb7a925d0f78008ea9b36db816d21fd4e9c5c7
Guillermo Rodríguez Cano <wschutz@gmail.com>
2017-11-18 10:29:28 +0100

    WIP: Symbolic link for ZSH home

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -12,7 +12,8 @@ DOTFILES_ZSH_DIR = $(DOTFILES_DIR)/zsh
 
 ZSH_BIN = $(shell command -v zsh 2> /dev/null)
 
-ZSH_PREZTO_HOME = $(DOTFILES_ZSH_DIR)/.zprezto
+ZSH_PREZTO = zprezto
+ZSH_PREZTO_HOME = $(DOTFILES_ZSH_DIR)/.$(ZSH_PREZTO)
 ZSH_PREZTO_FILES = zshenv zprofile zshrc zpreztorc zlogin zlogout
 
 #print-%  : ; @echo $* = $($*)
@@ -26,10 +27,11 @@ zsh_prezto_install:
 	mkdir -p $(DOTFILES_ZSH_DIR)
 	git clone --recursive https://github.com/sorin-ionescu/prezto.git "$(ZSH_PREZTO_HOME)"
 
-zsh_prezto_setup_config_files:
+zsh_prezto_configure:
 	$(foreach f, $(ZSH_PREZTO_FILES), ln -fs $(ZSH_PREZTO_HOME)/runcoms/$(f) $(DOTFILES_HOME)/.$(f);)
+	ln -fs $(ZSH_PREZTO_HOME) $(DOTFILES_HOME)/.$(ZSH_PREZTO)
 
-zsh_prezto: zsh_prezto_install zsh_prezto_setup_config_files
+zsh_prezto: zsh_prezto_install zsh_prezto_configure
 
 zsh_shell:
 	#sudo echo $(ZSH_BIN) >> /etc/shells
