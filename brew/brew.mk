30152cf2c87556de48f59b2d1cfd2e9619cb4590
Guillermo Rodríguez Cano <wileeam@users.noreply.github.com>
2020-02-04 16:34:45 +0100

    Changed specific shell in brew makefile to the shell function

--- brew/brew.mk
+++ brew/brew.mk
@@ -4,25 +4,24 @@
 .PHONY: brew brew_install brew_packages
 .SILENT: brew_install brew_packages
 
-BREW_BIN = $(shell command -v brew 2> /dev/null)
+BREW_BIN := $(shell command -v brew 2> /dev/null)
 BREW_URL := https://raw.githubusercontent.com/Homebrew/install/master/install
-BREW_INSTALL = $$(curl -fsSL $(BREW_URL))
+BREW_INSTALL := $$(curl -fsSL $(BREW_URL))
 
 DOTFILES_HOME ?= ${HOME}
-DOTFILES_DIR = $(DOTFILES_HOME)/.config
-DOTFILES_NVIM_DIR = $(DOTFILES_DIR)/nvim
+DOTFILES_DIR := $(DOTFILES_HOME)/.config
+DOTFILES_NVIM_DIR := $(DOTFILES_DIR)/nvim
 
 RUBY_BIN := $(shell command -v ruby 2> /dev/null)
 
 #print-%  : ; @echo $* = $($*)
 
 brew_install:
-ifneq ($(strip $(BREW_BIN)),)
+	echo "Installing Homebrew via URL: $(BREW_URL)"
 	$(RUBY_BIN) -e "$(BREW_INSTALL)"
-endif
 
 brew_packages:
 	brew install htop
 	brew install jq
-	
+
 brew: brew_install brew_packages
