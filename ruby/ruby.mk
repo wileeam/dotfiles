8481f1e82a15e7733407fdd3f3af68cd0550c6e5
Guillermo Rodríguez Cano <wileeam@users.noreply.github.com>
2020-03-28 20:54:24 +0100

    Ruby: Environment variable setup after rbenv installation

--- ruby/ruby.mk
+++ ruby/ruby.mk
@@ -21,11 +21,16 @@ ifeq ($(strip $(RBENV_BIN)),)
 endif
 
 rbenv_config_install:
-ifneq (,$(wildcard $DOTFILES_ZSHRC))
-	# @echo 'export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"' > ${DOTFILES_ZSHRC}.tmp
-	@false
-	# @cat ${DOTFILES_ZSHRC}.tmp >> ${DOTFILES_ZSHRC}
+	@echo '# Ruby environment variable (installed due to rbenv)' > ${DOTFILES_ZSHRC}.tmp
+	@echo 'export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"' >> ${DOTFILES_ZSHRC}.tmp
+ifeq ($(wildcard $DOTFILES_ZSHRC),) # File does not exist
+	@echo '${DOTFILES_ZSHRC} does not exist. Creating it and adding environment variable...'
+	@cp ${DOTFILES_ZSHRC}.tmp ${DOTFILES_ZSHRC}
+else # File does exist
+	@echo '${DOTFILES_ZSHRC} does exist. Adding environment variable...'
+	@cat ${DOTFILES_ZSHRC}.tmp >> ${DOTFILES_ZSHRC}
 endif
+	@rm ${DOTFILES_ZSHRC}.tmp
 
 rbenv:
 ifneq ($(strip $(BREW_BIN)),)
