98dcf4d29c3025fd156d3c391ad6c1ad5d76b328
Guillermo Rodríguez Cano <wileeam@users.noreply.github.com>
2020-11-17 14:29:21 +0100

    Brew: Updated to using Bash instead of Ruby

--- brew/brew.mk
+++ brew/brew.mk
@@ -5,20 +5,20 @@
 .SILENT: brew_install brew_packages
 
 BREW_BIN := $(shell command -v brew 2> /dev/null)
-BREW_URL := https://raw.githubusercontent.com/Homebrew/install/master/install
+BREW_URL := https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 BREW_INSTALL := $$(curl -fsSL $(BREW_URL))
 
 DOTFILES_HOME ?= ${HOME}
 DOTFILES_DIR := $(DOTFILES_HOME)/.config
 DOTFILES_NVIM_DIR := $(DOTFILES_DIR)/nvim
 
-RUBY_BIN := $(shell command -v ruby 2> /dev/null)
+BASH_BIN := $(shell command -v bash 2> /dev/null)
 
 #print-%  : ; @echo $* = $($*)
 
 brew_install:
 	echo "Installing Homebrew via URL: $(BREW_URL)"
-	$(RUBY_BIN) -e "$(BREW_INSTALL)"
+	$(BASH_BIN) -c "$(BREW_INSTALL)"
 
 brew_packages:
 	brew install htop
